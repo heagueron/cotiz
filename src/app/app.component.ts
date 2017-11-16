3a9e3675bebe84a40bf1ccb1826aaf65b38aa267
Hector Aguero <heagueron@gmail.com>
2017-11-16 13:14:58 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { CotizacionComponent } from './cotizacion/cotizacion.component'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //title = 'Cotiz  Version 1.0.0';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CotizacionComponent } from './cotizacion/cotizacion.component';
+
+import { DataService } from './data.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CotizacionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
